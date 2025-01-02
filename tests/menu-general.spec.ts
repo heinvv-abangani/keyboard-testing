3f1442eeda4dc12aa4eb1d3fb5a83d5e3ee00acc
Hein van Vlastuin <heinvanvlastuin@gmail.com>
2025-01-02 10:47:23 +0200

    wip

--- tests/menu-general.spec.ts
+++ tests/menu-general.spec.ts
@@ -1,12 +1,22 @@
 import { test, expect } from '@playwright/test';
 
+const testWebsites = [
+    'https://academy.bricksbuilder.io/article/menu-builder/',
+    'https://labelvier.nl/',
+    'https://www.d-tec.eu/nl',
+    'https://www.spankrachtontwerpers.nl/',
+];
+
 test('count the number of menus on a page', async ({ page }) => {
-    await page.goto('https://labelvier.nl/');
+    for (let websiteIndex = 0; websiteIndex < testWebsites.length; websiteIndex++) {
+        await page.goto(testWebsites[websiteIndex]);
 
-    const menusInsideHeader = page.locator('header nav:visible');
-
-    await iterateMenus(menusInsideHeader);
+        const menusInsideHeader = page.locator('header nav:visible');
+    
+        await iterateMenus(menusInsideHeader);
+    }
 });
+
 
 async function isElementTrulyVisible(element) {
     // Check if the element is attached to the DOM
@@ -48,11 +58,14 @@ async function isElementTrulyVisible(element) {
 async function iterateMenus(menus) {
     const menuCount = await menus.count();
 
+    let visibleMenuCount = 0;
+
     console.log( 'menu count' + menuCount );
 
     for (let i = 0; i < menuCount; i++) {
         const menuItem = menus.nth(i);
         const isMenuItemVisible = await isElementTrulyVisible(menuItem);
+        
         console.log(`Menu ${i + 1}: Truly Visible = ${isMenuItemVisible}`);
 
         const links = menuItem.locator('a');
@@ -62,6 +75,7 @@ async function iterateMenus(menus) {
         // Steps:
         // Count number of visible and invisible menu items
         // If all items are visible, end test.
+
         // Else:
             // Select all visible button[aria-expanded] elements on level 1.
             // Focus each button.
@@ -76,13 +90,27 @@ async function iterateMenus(menus) {
 }
 
 async function iterateMenuItems( links ) {
-    const linkCount = await links.count();
+    const menuItemCount = await links.count();
 
-    for (let j = 0; j < linkCount; j++) {
+    let visibleMenuItemCount = 0;
+
+    for (let j = 0; j < menuItemCount; j++) {
         const link = links.nth(j);
         const linkText = (await link.textContent())?.trim();
         const href = await link.getAttribute('href');
         const isLinkVisible = await isElementTrulyVisible(link);
+
+        if ( isLinkVisible ) {
+            visibleMenuItemCount++;
+        }
+   
         console.log(`    Link ${j + 1}: Text = ${linkText}, Href = ${href}, Truly Visible = ${isLinkVisible}`);
+    }
+
+    console.log(`Menu items: ${menuItemCount}, Visible menu items = ${visibleMenuItemCount}`);
+    
+
+    if ( visibleMenuItemCount === menuItemCount ) {
+        return;
     }
 }
